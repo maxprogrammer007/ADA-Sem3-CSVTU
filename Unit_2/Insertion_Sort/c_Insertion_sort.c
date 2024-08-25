15cde1e3ae4beb59858f5ab4023a24e32372bdf9
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-08-25 09:59:56 +0530

    Create c_Insertion_sort.c

---
+++ Unit_2/Insertion_Sort/c_Insertion_sort.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void insertion_sort(int a[] , int length){
+    for (int i = 1 ; i < length ; i++){
+        int key = a[i];
+        int j = i - 1;
+
+        
+    }
+}
+
+int main () {
+    int a[] = {5, 2, 4, 6, 1, 3};
+
+}
