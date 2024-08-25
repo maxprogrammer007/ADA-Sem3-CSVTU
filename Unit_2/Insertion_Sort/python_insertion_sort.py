c0dc5f29c4931f2b5fc27e359e7885f79e010c94
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-08-25 18:48:38 +0530

    Update python_insertion_sort.py

--- Unit_2/Insertion_Sort/python_insertion_sort.py
+++ Unit_2/Insertion_Sort/python_insertion_sort.py
@@ -11,3 +11,8 @@ def insertionSort(lst):
 if __name__ == '__main__':
     lst = [64, 88, 51, 65, 90, 75, 34, 79, 46, 36]
     print(insertionSort(lst))
+
+# 'if __name__ == "__main__":' ka use isliye karte hain taaki yeh code sirf tabhi chale jab script directly run ki jaye.
+# Agar hum is script ko kisi dusre Python file mein import karte hain, toh yeh code automatically execute nahi hoga.
+# Yeh practice useful hai jab hum code ko reuse karna chahte hain, bina kisi unwanted execution ke.
+# Isse script ko modular aur maintainable banaya ja sakta hai, jahan testing ya specific code sirf direct execution pe hi chalega.
